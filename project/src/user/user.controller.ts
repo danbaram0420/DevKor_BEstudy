7d204011b7c3ee7f9b3e6a1cb049f53c1177c3c1
Park SeungHyun <danbaram0420@korea.ac.kr>
2024-05-01 20:01:00 +0900

    4주차

--- project/src/user/user.controller.ts
+++ project/src/user/user.controller.ts
@@ -1,32 +1,42 @@
-import { Controller, Get, Post , Delete , Param, Body} from '@nestjs/common';
+import { Controller, Get, Post , Delete , Put, Body, UseFilters} from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserEntity } from 'src/entities';
+import { HttpExceptionFilter } from 'src/filter/http-exception.filter';
 
 @Controller('user')//user라는 경로로 들어오는 요청을 처리하는 컨트롤러. 루트 경로에 /user를 붙여서 들어오면 이 컨트롤러가 실행, 아래의 모든 함수들에 적용됨.
+@UseFilters(HttpExceptionFilter)//예외처리를 위한 filter를 사용함. 이 컨트롤러의 모든 함수에 적용됨.
+
 export class UserController {
     constructor(private readonly userService: UserService) {}
-
     @Get()
-    getUsers(): string[] {//굳이 여기 함수 이름과 userService의 함수 이름이 같을 필요는 없음. 다르게 적어도 됨.
-        return this.userService.getUsers();
+    async getUsers(): Promise<UserEntity[]> {//굳이 여기 함수 이름과 userService의 함수 이름이 같을 필요는 없음. 다르게 적어도 됨.
+        return await this.userService.getUsers();//async함수는 Promise로 리턴값 타입 약속
     }
 
-    @Get('/:index')
-    getUserName(@Param('index') index: number): string {//param은 경로에 있는 변수를 가져옴. user뒤에 붙은 index(:index 아니다!)를 가져와서 number로 변환함.
-        return this.userService.getUserName(index);
+    @Get('/find')
+    async findByEmail(@Body() info) {
+        return await this.userService.findByEmail(info.email);
     }
 
     //위 함수 이후 @Get('추가 경로')가 있다면 위 함수인 index로 받아들여 typeerror가 발생할 수 있다.
     //해결하는 방법은 그 함수를 위 함수보다 위에 적어주는 것이다.
 
+    //nestjs는 리턴할 때 계산 완료된 상황에서 리턴하기에 await를 굳이 붙일 필요는 없다.
+    //다만 디버깅할 때 await를 붙이면 디버깅이 더 쉬울 수 있다.
+
     @Post()
-    addUser(@Body() info): Promise<string[]> {//body에 있는 정보를 받아서 Promise<string[]>을 반환함.
-        return this.userService.addUser(info.name);//userService의 addUser 함수에 info.name을 넣어서 실행함.
+    async addUser(@Body() info) {//body에 있는 정보를 받음
+        return await this.userService.addUser(info);//userService의 addUser 함수에 info를 넣어서 실행함.
     }
 
     @Delete()
-    deleteUser(@Body() info): Promise<string[]> {
-        return this.userService.deleteUser(info.name);
+    deleteUser(@Body() info) {
+        return this.userService.deleteUser(info.email);
     }
 
+    @Put()
+    updatePassword(@Body() info) {
+        return this.userService.updatePassword(info.email, info.password);
+    }
 
 }
--- project/src/user/user.service.ts
+++ project/src/user/user.service.ts
@@ -1,31 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+
+    constructor(
+        @InjectRepository(UserEntity)//entity는 바로 사용할 수 없어서 repository로 변환해 줌.
+        private readonly userRepository: Repository<UserEntity>,
+    ) {}
+
     users: string[] = ['Alice', 'Bob', 'Eve'];//users는 string 배열. 말했듯, 변수 또는 함수의 타입을 뒤에 적어줌.
 
-    getUsers(): string[] {//getUsers는 string 배열을 반환함.
-        return this.users;//UserService 클래스(자기 자신)의 users를 반환함.
+    async getUsers(): Promise<UserEntity[]> {//getUsers는 UserEntity 배열을 반환함.
+        const users = await this.userRepository.find();//userRepository의 find 실행. 
+        return users;//find는 userRepository의 모든 데이터를 찾아서 반환함. 즉, 모든 유저 정보를 반환함.
     }
 
-    async addUser(name: string): Promise<string[]> {//addUser 함수는 name이라는 변수의 string을 받아서 Promise<string[]>을 반환함.
-        //Promise는 비동기라 string을 반환할 것을 확신할 수 없기에 string으로 반환될 것이라고 약속하는 것(비동기일 때 promise를 사용함.)
+    async findByEmail(email: string): Promise<UserEntity> {
+        const user = await this.userRepository.findOne({ where: {email} });//userRepository의 findOne 실행. email이 일치하는 유저를 찾아서 반환함.
+        return user;//찾은 유저를 반환함.
+    }
+
+    async addUser(info: UserEntity): Promise<UserEntity> {//addUser 함수는 info이라는 변수의 UserEntity를 받아서 UserEntity반환.
+        //Promise는 비동기라 반환 타입을 확정지을 수 없어 UserEntity 반환을 '약속'함
         //update관련 함수는 대부분 비동기로 설정함.
-        if (!this.users.includes(name)) {//users 배열에 name이 없으면
-            await this.users.push(name);//name을 users 배열에 추가함.
+        if (!(await this.findByEmail(info.email))) {//users 배열에 email로 찾은 name이 없으면
+            //함수명에 마우스 대 봤을 때 반환타입이 Promise로 지정되어 있으면 await를 사용해야 함.
+            const user = this.userRepository.create(info);//userRepository에 info를 생성함.
+            //만약 데이터를 entity로 받지 않고 따로따로 받더라도 create를 통해 entity를 만들어서 저장할 수 있음.
+            //이 함수는 UserEntity를 받기 때문에 딱히 쓸 데는 없는 코드긴 함
+            return await this.userRepository.save(user);//userRepository에 user를 저장함.
         }
-        return this.users;//users 배열을 반환함.
+        else {
+            throw new HttpException('User already exists', 409);//이미 존재하는 유저라는 에러를 반환함.
+        }
     }
 
-    async deleteUser(name: string): Promise<string[]> {
-        if (this.users.includes(name)) {//users 배열에 name이 있으면
-            await this.users.splice(this.users.indexOf(name), 1);//users 배열에서 name을 제거함.
+    async deleteUser(email: string): Promise<void> {//삭제는 반환값이 없음
+        if (await this.findByEmail(email)) {//users 배열에 email로 찾은 name이 있으면
+            await this.userRepository.delete({email});//userRepository에서 email이 일치하는 유저를 삭제함.
         }
-        return this.users;//users 배열을 반환함.
+        else {
+            throw new HttpException('User not found', 404);//유저를 찾을 수 없다는 에러를 반환함.
+        }
     }
 
-    getUserName(index: number): string {//동기, 비동기 설정은 크게 관련이 없는 함수들은 동기로 설정해도 상관없다. 다 async로 설정해도 상관없긴 한데 시간이 오래 걸린다.
-        return this.users[index];//users 배열에서 num번째 요소를 반환함.
+    async updatePassword(email: string, password: string): Promise<void> {
+        if (await this.findByEmail(email)) {//users 배열에 email로 찾은 name이 있으면
+            await this.userRepository.update({email}, {password});//userRepository에서 email이 일치하는 유저의 password를 변경함.
+        }//입력으로 받는 변수명이 UserEntity의 변수명과 같아 그대로 사용, 다를 경우 {email: 변수명} 이런 식으로 사용.
+        else {
+            throw new HttpException('User not found', 404);//유저를 찾을 수 없다는 에러를 반환함.
+        }
     }
 
 }
